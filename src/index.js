5a52414358d8a41158292e21cd0a8581c776e8cd
bmangeat <brice.mangeat@gmail.com>
2020-12-29 16:10:35 +0100

    :sparkles: Create basic scene

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import './styles.css'
+import Scene from "./three/Scene"
+
+window.scene = new Scene()
---
+++ src/three/Scene.js
@@ -0,0 +1,56 @@
+import * as THREE from 'three'
+
+const perspective = 800
+
+export default class Scene {
+    constructor() {
+        this.container = document.getElementById('stage')
+
+        this.scene = new THREE.Scene()
+        this.scene.background = new THREE.Color(0x212121 )
+
+        this.renderer = new THREE.WebGLRenderer({
+            canvas: this.container,
+            alpha: true
+        })
+
+        this.renderer.setSize(window.innerWidth, window.innerHeight)
+        this.renderer.setPixelRatio(window.devicePixelRatio)
+
+        this.initCamera()
+        this.addEvent()
+    }
+
+    initCamera(){
+
+        const w = window.innerWidth;
+        const h = window.innerHeight;
+        const fov = (180 * (2 * Math.atan( h / 2 / perspective ))) / Math.PI
+
+        this.camera = new THREE.PerspectiveCamera(fov, w/h, 0.1, 1000)
+        this.camera.position.set(0, 0, perspective)
+    }
+
+
+    onWindowResize() {
+        const w = window.innerWidth;
+        const h = window.innerHeight;
+
+        this.camera.aspect = w / h;
+        this.camera.updateProjectionMatrix();
+
+        this.renderer.setSize( w, h );
+    }
+
+    addEvent() {
+        window.requestAnimationFrame(this.update.bind(this))
+        window.addEventListener( "resize", this.onWindowResize.bind( this ), false );
+    }
+
+    update(){
+        if ( this.renderer === undefined ) return
+        requestAnimationFrame( this.update.bind( this ) )
+
+        this.renderer.render( this.scene, this.camera )
+    }
+}
